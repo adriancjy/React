d353504f88cce2ef681e7c09b4aa59f7bb11a834
adriancjy <adriantdh@live.com>
2020-01-07 16:55:54 +0800

    updated aggregated page.

--- src/views/charts/chartjs/chart.js
+++ src/views/charts/chartjs/chart.js
@@ -44,26 +44,9 @@ const ChartJS = props => (
                         </CardTitle>
                         <p>
                            List of items in which the order does explicitly
-                           matter.
+                           matter. [Placeholder for recommended products]
                         </p>
-                        <ol>
-                           <li>Lorem ipsum dolor sit amet</li>
-                           <li>Consectetur adipiscing elit</li>
-                           <li>Integer molestie lorem at massa</li>
-                           <li>Facilisis in pretium nisl aliquet</li>
-                           <li>
-                              Nulla volutpat aliquam velit
-                              <ol>
-                                 <li>Phasellus iaculis neque</li>
-                                 <li>Purus sodales ultricies</li>
-                                 <li>Vestibulum laoreet porttitor sem</li>
-                                 <li>Ac tristique libero volutpat at</li>
-                              </ol>
-                           </li>
-                           <li>Faucibus porta lacus fringilla vel</li>
-                           <li>Aenean sit amet erat nunc</li>
-                           <li>Eget porttitor lorem</li>
-                        </ol>
+                        
                      </CardBody>
                   </Card>
                </Col>
