2cec6fcd2a3ad93f3abc74106769e9e1fe73cccc
Tan De Hui Adrian <1801050@sit.singaporetech.edu.sg>
2020-01-07 14:01:10 +0800

    edit table

--- src/views/tables/examples/tableHover.js
+++ src/views/tables/examples/tableHover.js
@@ -4,13 +4,44 @@ import { Table } from 'reactstrap';
 
 export default class Example extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      types: [],
+      amounts: [],
+      dates: [],
+      tags: [],
+      referenceNumbers: []
+    }
+  }
+
   async componentWillMount() {
 
     try 
     {
       console.log(this.props.data[0]);
       if(this.props.data[0]){
-        console.log(this.props.data[0].type);
+
+          var types = []
+          var amounts = []
+          var dates = []
+          var tags = []
+          var referenceNumbers = []
+          
+          var i;
+          for(i=0; i<this.props.data.length ; i++ ) {
+            types.push(this.props.data[i]['type'])
+            amounts.push(this.props.data[i]['amount'])
+            dates.push(this.props.data[i]['date'])
+            tags.push(this.props.data[i]['tag'])
+            referenceNumbers.push(this.props.data[i]['referenceNumber'])
+          }
+          this.setState({types});
+          this.setState({amounts});
+          this.setState({dates});
+          this.setState({tags});
+          this.setState({referenceNumbers});
+
       }
     }
     catch (e) 
